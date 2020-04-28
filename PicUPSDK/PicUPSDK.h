831a14cca8680b4ebeb72ba903664f625f1fba2a
Vitaliy Kislyy <vitaliykua@gmail.com>
2020-04-28 15:48:43 +0300

    added info.plist

---
+++ PicUPSDK/PicUPSDK.h
@@ -0,0 +1,19 @@
+//
+//  PicUPSDK.h
+//  PicUPSDK
+//
+//  Created by Admin on 25.11.2019.
+//  Copyright © 2019 ItWorksinUA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PicUPSDK.
+FOUNDATION_EXPORT double PicUPSDKVersionNumber;
+
+//! Project version string for PicUPSDK.
+FOUNDATION_EXPORT const unsigned char PicUPSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PicUPSDK/PublicHeader.h>
+
+
